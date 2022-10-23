26cabdd0080a4c3441f000b5d3c0c6ad332e7064
tanjid <tanjid.du@gmail.com>
2022-10-23 17:28:46 +0600

    dad

--- _keenthemes/settings.py
+++ _keenthemes/settings.py
@@ -190,8 +190,8 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Media File Configuratuon
-MEDIA_URL = '/assets/'
-MEDIA_ROOT = BASE_DIR /'assets'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
