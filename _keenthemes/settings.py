e48e273262bdf21b26c9e1db72817ca1af2bfc2a
tanjid <tanjid.du@gmail.com>
2022-10-24 13:35:08 +0600

    medi

--- _keenthemes/settings.py
+++ _keenthemes/settings.py
@@ -191,9 +191,15 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Media File Configuratuon
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR /'media'
+# MEDIA_ROOT = BASE_DIR /'media'
+MEDIA_ROOT = 'https://octopus-app-8vb35.ondigitalocean.app/media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+
+
+
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
