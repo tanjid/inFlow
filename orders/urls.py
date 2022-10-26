f459211df715430d2e01a82e3040951a4811c89c
tanjid <tanjid.du@gmail.com>
2022-10-26 18:26:46 +0600

    Update urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -1,16 +1,14 @@
 from django.urls import path
-from .views import TestMessage, OrderListView, NewOrderView, OrderHome, CompleteListView, ReturnListView, CancelListView, OrderDm, EditOrder, confirm_order, SingleConfirmView, ExchangeOrder, ReturnOrder, return_single, confirm_single, cancel_order, ExchangeItems
-from . import views
+from .views import OrderListView, NewOrderView, OrderHome, CompleteListView, ReturnListView, CancelListView, OrderDm, EditOrder, confirm_order, SingleConfirmView, ExchangeOrder, ReturnOrder, return_single, confirm_single, cancel_order, ExchangeItems, load_order_data, load_mobile_data
 
 
 urlpatterns = [
     # ...
 
-    path('test_message/', TestMessage.as_view(), name='test_message'),
-    path('', OrderListView.as_view(), name='order_list'),
+    # path('test_message/', TestMessage.as_view(), name='test_message'),
     path('new_order/', NewOrderView.as_view(), name='new_order'),
-    path('load_order_data/', views.load_order_data, name='load_order_data'),
-    path('load_mobile_data/<str:mobile_number>', views.load_mobile_data, name='load_mobile_data'),
+    path('load_order_data/', load_order_data, name='load_order_data'),
+    path('load_mobile_data/<str:mobile_number>', load_mobile_data, name='load_mobile_data'),
     path('order_home/', OrderHome.as_view(), name='order_home'),
 
     path('order_list/<str:dm>/Complete', CompleteListView.as_view(), name='complete_list'),
